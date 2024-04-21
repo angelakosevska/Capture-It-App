b0b27306d89392cade5870f598d6f383c1563b9f
Angela <you@example.com>
2024-04-21 20:47:20 +0200

    folder struktura master branch osnovno

--- src/pages/AlbumPage/index.jsx
+++ src/pages/AlbumPage/index.jsx
@@ -1,8 +1,7 @@
 import { Layout } from "../../UI/Layout.jsx";
-import "./Album.css";
-import { AlbumPicture } from "../components/Picture/AlbumPicture";
+
 import PictureAndUsername from "../../components/PictureAndUsername/index.jsx";
-import { PrivateEvent } from "../components/Picture/PrivateEvent";
+
 import Breadcrumbs from "../../components/BreadCrumbs/index.jsx";
 import  Like  from "../../components/Like/index.jsx";
 export function Album() {
@@ -15,26 +14,9 @@ export function Album() {
               <Breadcrumbs />
             </div>
             <div className="album-grid">
-              <div>
-                <PrivateEvent />
-              </div>
-              <div>
-                <AlbumPicture imageUrl="https://images.pexels.com/photos/6794043/pexels-photo-6794043.jpeg?cs=srgb&dl=pexels-leeloo-the-first-6794043.jpg&fm=jpg" />
-              </div>
-              <div>
-                <AlbumPicture imageUrl="https://images.pexels.com/photos/6794043/pexels-photo-6794043.jpeg?cs=srgb&dl=pexels-leeloo-the-first-6794043.jpg&fm=jpg" />
-              </div>
-              <div>
-                <AlbumPicture imageUrl="https://images.pexels.com/photos/6794043/pexels-photo-6794043.jpeg?cs=srgb&dl=pexels-leeloo-the-first-6794043.jpg&fm=jpg" />
-              </div>
-              <AlbumPicture imageUrl="https://images.pexels.com/photos/6794043/pexels-photo-6794043.jpeg?cs=srgb&dl=pexels-leeloo-the-first-6794043.jpg&fm=jpg" />
-              <div>
-                <AlbumPicture imageUrl="https://images.pexels.com/photos/6794043/pexels-photo-6794043.jpeg?cs=srgb&dl=pexels-leeloo-the-first-6794043.jpg&fm=jpg" />
-              </div>
-              <div>
-                <AlbumPicture imageUrl="https://images.pexels.com/photos/6794043/pexels-photo-6794043.jpeg?cs=srgb&dl=pexels-leeloo-the-first-6794043.jpg&fm=jpg" />
-              </div>
-              <Like />
+        
+               
+         
             </div>
           </main>
         </body>
--- src/pages/EventPage/index.jsx
+++ src/pages/EventPage/index.jsx
@@ -2,37 +2,12 @@ import { Link } from "react-router-dom";
 import { Layout } from "../../UI/Layout.jsx";
 import  Like from "../../components/Like/index.jsx";
 import Comment from "../../components/Comment/index.jsx";
-import "./event.css";
-import { AlbumPicture } from "../components/Picture/AlbumPicture";
+import "./style.css";
 export function Event() {
   return (
     <>
       <Layout>
-        <div className="containerE">
-          <div className="picture">
-            <AlbumPicture />
-          </div>
-          <div className="picture">
-            <AlbumPicture />
-          </div>
-          <div className="picture">
-            <AlbumPicture />
-          </div>
-          <div className="picture">
-            <AlbumPicture />
-          </div>
-          <div className="picture">
-            <AlbumPicture />
-          </div>
-          <div className="picture">
-            <AlbumPicture />
-          </div>
-          <div className="picture">
-            <AlbumPicture />
-          </div>
-        </div>
-
-        <aside></aside>
+         
       </Layout>
     </>
   );
