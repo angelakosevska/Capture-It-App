a4aba95fc9b5ed8ee91eb21485bd4e5c008f50c3
andrea <pavlevska.andrea@uklo.edu.mk>
2024-06-01 17:24:32 +0200

    andrea commit

--- src/components/Routing/index.jsx
+++ src/components/Routing/index.jsx
@@ -3,30 +3,46 @@ import { Home } from "../../pages/HomePage/index.jsx";
 import { Profile } from "../../pages/profile.jsx";
 import { Album } from "../../pages/AlbumPage/index.jsx";
 import { Event } from "../../pages/EventPage/index.jsx";
+import Login from "../../pages/LogInPage/index.jsx";
 import Footer from "../Footer/index.jsx";
 import Header from "../Header/index.jsx";
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-const Routing = () => {
-  const Layout = () => {
-    return (
-      <>
-        <Header />
-        <Outlet />
-        <Footer />
-      </>
-    );
-  };
 
+const Layout = () => {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/event" element={<Event />} />
-        <Route path="/album" element={<Album />} />
-      </Route>
-    </Routes>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const NoLayout = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+const Routing = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/album" element={<Album />} />
+        </Route>
+        <Route path="/login" element={<NoLayout />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+        {/* Add register route if needed */}
+        {/* <Route path="/register" element={<NoLayout />}>
+          <Route path="/register" element={<Register />} />
+        </Route> */}
+      </Routes>
     </>
   );
 };
