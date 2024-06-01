fd32c775ac534616c079967791e697cca244927d
Angela <you@example.com>
2024-06-01 19:34:08 +0200

    header dropdown

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,111 @@
+// import React, { useState } from "react";
+// import { Link, useHistory } from "react-router-dom";
+// import "bootstrap-icons/font/bootstrap-icons.css";
+// import Logo1 from "../../Logo";
+// import "./style.css";
+
+// const Header = ({ user }) => {
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const [showDropdown, setShowDropdown] = useState(false);
+
+//   const suggestions = ["John Doe", "Jane Smith", "Event Name"];
+
+//   const handleProfileClick = () => {
+//     setShowDropdown(!showDropdown);
+//   };
+
+//   return (
+//     <header>
+//       <div className="Logo">
+//         <Link to="/">
+//           <svg
+//             width="40"
+//             height="40"
+//             viewBox="0 0 40 40"
+//             fill="none"
+//             xmlns="http://www.w3.org/2000/svg"
+//           >
+//            <Logo1/>
+//           </svg>
+//         </Link>
+//         <div className="LogoText">
+//           <Link to="/">Capture It</Link>
+//         </div>
+//       </div>
+//       <div className="SearchBar">
+//         <div className="menuAndItem">
+//           <input
+//             type="text"
+//             placeholder="Search..."
+//             value={searchTerm}
+//             onChange={(e) => {
+//               setSearchTerm(e.target.value);
+//               setShowDropdown(true);
+//             }}
+//             onFocus={() => setShowDropdown(true)}
+//             onBlur={() => setShowDropdown(false)}
+//           /> 
+//           {false && ( /*ova da se meni */
+//             <div className="SearchDropdownMenu">
+//               {suggestions
+//                 .filter((suggestion) =>
+//                   suggestion.toLowerCase().includes(searchTerm.toLowerCase())
+//                 )
+//                 .map((suggestion, index) => (
+//                   <div key={index} className="SearchDropdownItem">
+//                     {suggestion}
+//                   </div>
+//                 ))}
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//       <div className="CreateEvent">
+//         <Link to="/event">
+//           <i className="bi bi-plus-circle"></i>
+//         </Link>
+//       </div>
+//       <div className="ProfileLink" onClick={handleProfileClick}>
+//         <i className="bi bi-person-circle"></i>
+//         {showDropdown && ( /* i ova */
+//           <div className="ProfileDropdownMenu">
+//             <div className="dropdown-item">{user?.firstName}</div>
+//             <div className="dropdown-item">{user?.lastName}</div>
+//             <div className="dropdown-item">Settings</div>
+//             <hr />
+//             <div className="ProfileDropdownItem">
+//               <Link to="/Login">Log Out</Link>
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
 import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Logo1 from "../../Logo";
 import "./style.css";
 
 const Header = ({ user }) => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [showDropdown, setShowDropdown] = useState(false);
+  const [showSearchDropdown, setShowSearchDropdown] = useState(false);
+  const [showProfileDropdown, setShowProfileDropdown] = useState(false);
 
   const suggestions = ["John Doe", "Jane Smith", "Event Name"];
 
   const handleProfileClick = () => {
-    setShowDropdown(!showDropdown);
+    setShowProfileDropdown(!showProfileDropdown);
   };
 
   return (
     <header>
       <div className="Logo">
-        <Link to="/">
+        <Link to="/Home">
           <svg
             width="40"
             height="40"
@@ -25,7 +113,7 @@ const Header = ({ user }) => {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-           <Logo1/>
+            <Logo1 />
           </svg>
         </Link>
         <div className="LogoText">
@@ -40,12 +128,12 @@ const Header = ({ user }) => {
             value={searchTerm}
             onChange={(e) => {
               setSearchTerm(e.target.value);
-              setShowDropdown(true);
+              setShowSearchDropdown(true);
             }}
-            onFocus={() => setShowDropdown(true)}
-            onBlur={() => setShowDropdown(false)}
+            onFocus={() => setShowSearchDropdown(true)}
+            onBlur={() => setShowSearchDropdown(false)}
           /> 
-          {false && ( /*ova da se meni */
+          {showSearchDropdown && (
             <div className="SearchDropdownMenu">
               {suggestions
                 .filter((suggestion) =>
@@ -67,7 +155,7 @@ const Header = ({ user }) => {
       </div>
       <div className="ProfileLink" onClick={handleProfileClick}>
         <i className="bi bi-person-circle"></i>
-        {showDropdown && ( /* i ova */
+        {showProfileDropdown && (
           <div className="ProfileDropdownMenu">
             <div className="dropdown-item">{user?.firstName}</div>
             <div className="dropdown-item">{user?.lastName}</div>
