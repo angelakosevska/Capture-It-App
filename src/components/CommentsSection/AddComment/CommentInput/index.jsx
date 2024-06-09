90fd21f50e3509761f3f4d28fad4b9909babc19e
Angela <you@example.com>
2024-06-09 13:18:23 +0200

    event album cards rabotat

--- src/components/CommentsSection/AddComment/CommentInput/index.jsx
+++ src/components/CommentsSection/AddComment/CommentInput/index.jsx
@@ -13,7 +13,7 @@ const CommentInput = ({
   buttonWidth,
   buttonHeight,
   pictureId,
-  onCommentAdded
+  onCommentAdded,
 }) => {
   const [commentBody, setCommentBody] = useState("");
 
@@ -29,7 +29,7 @@ const CommentInput = ({
           {
             headers: {
               Authorization:
-                "Bearer  eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoia29zZXZza2FhIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiIxMSIsImV4cCI6MTcxNzc5ODM1OH0.zSyPcsIZcL9NFBinzhvC1sZNMtN24FAMYpQGTXdFR1k",
+                "Bearer  eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoia29zZXZza2FhIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiIxMSIsImV4cCI6MTcxNzkzNTExNH0.Q4YIY-3zIwtv8Q8HK2LTc8WgzSEgxnDnZPTbD9bIuhM",
             },
           }
         );
--- src/pages/EventPage/index.jsx
+++ src/pages/EventPage/index.jsx
@@ -41,7 +41,7 @@ export function Event() {
         {
           headers: {
             Authorization:
-              " Bearer  eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoia29zZXZza2FhIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiIxMSIsImV4cCI6MTcxNzc5ODM1OH0.zSyPcsIZcL9NFBinzhvC1sZNMtN24FAMYpQGTXdFR1k",
+              " Bearer  eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoia29zZXZza2FhIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiIxMSIsImV4cCI6MTcxNzkzNTExNH0.Q4YIY-3zIwtv8Q8HK2LTc8WgzSEgxnDnZPTbD9bIuhM",
           },
         }
       );
